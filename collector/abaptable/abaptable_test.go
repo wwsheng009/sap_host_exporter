8ae2089fb658bc307c94143d9ba70957dde98872
wwsheng009 <vincentwwsheng@gmail.com>
2024-11-21 12:00:27 +0800

    update test case file

--- collector/abaptable/abaptable_test.go
+++ collector/abaptable/abaptable_test.go
@@ -21,12 +21,12 @@ func TestNewCollector(t *testing.T) {
 	assert.Nil(t, err)
 }
 
-func TestProcessesMetric(t *testing.T) {
+func TestWorkProcessesMetric(t *testing.T) {
 	ctrl := gomock.NewController(t)
 	defer ctrl.Finish()
 
 	mockWebService := mock_sapcontrol.NewMockWebService(ctrl)
-	mockWebService.EXPECT().GetProcessList().Return(&sapcontrol.GetABAPWPTableResponse{
+	mockWebService.EXPECT().GetABAPWPTable().Return(&sapcontrol.GetABAPWPTableResponse{
 		Workprocess: &sapcontrol.ArrayOfWorkProcess{
 			Item: []*sapcontrol.WorkProcess{
 				{
@@ -48,7 +48,7 @@ func TestProcessesMetric(t *testing.T) {
 			},
 		},
 	}, nil)
-	mockWebService.EXPECT().GetSystemInstanceList().Return(&sapcontrol.GetSystemInstanceListResponse{}, nil)
+	// mockWebService.EXPECT().GetSystemInstanceList().Return(&sapcontrol.GetSystemInstanceListResponse{}, nil)
 	mockWebService.EXPECT().GetCurrentInstance().Return(&sapcontrol.CurrentSapInstance{
 		SID:      "S4H",
 		Number:   0,
@@ -67,54 +67,5 @@ func TestProcessesMetric(t *testing.T) {
 	assert.NoError(t, err)
 
 	err = testutil.CollectAndCompare(collector, strings.NewReader(expectedMetrics), "sap_start_service_processes")
-	assert.NoError(t, err)
-}
-
-func TestInstancesMetric(t *testing.T) {
-	ctrl := gomock.NewController(t)
-	defer ctrl.Finish()
-
-	mockWebService := mock_sapcontrol.NewMockWebService(ctrl)
-	mockWebService.EXPECT().GetSystemInstanceList().Return(&sapcontrol.GetSystemInstanceListResponse{
-		Instances: []*sapcontrol.SAPInstance{
-			{
-				Hostname:      "sapha1as",
-				InstanceNr:    0,
-				HttpPort:      50013,
-				HttpsPort:     50014,
-				StartPriority: "1",
-				Features:      "MESSAGESERVER|ENQUE",
-				Dispstatus:    sapcontrol.STATECOLOR_GREEN,
-			},
-			{
-				Hostname:      "sapha1er",
-				InstanceNr:    10,
-				HttpPort:      51013,
-				HttpsPort:     51014,
-				StartPriority: "0.5",
-				Features:      "ENQREP",
-				Dispstatus:    sapcontrol.STATECOLOR_GREEN,
-			},
-		},
-	}, nil)
-	mockWebService.EXPECT().GetProcessList().Return(&sapcontrol.GetProcessListResponse{}, nil)
-	mockWebService.EXPECT().GetCurrentInstance().Return(&sapcontrol.CurrentSapInstance{
-		SID:      "HA1",
-		Number:   0,
-		Name:     "ASCS",
-		Hostname: "sapha1as",
-	}, nil).AnyTimes()
-
-	expectedMetrics := `
-	# HELP sap_start_service_instances The SAP instances in the context of the whole SAP system
-	# TYPE sap_start_service_instances gauge
-    sap_start_service_instances{SID="HA1",features="MESSAGESERVER|ENQUE",instance_hostname="sapha1as",instance_name="ASCS",instance_number="0",start_priority="1"} 2
-	`
-
-	var err error
-	collector, err := NewCollector(mockWebService)
-	assert.NoError(t, err)
-
-	err = testutil.CollectAndCompare(collector, strings.NewReader(expectedMetrics), "sap_start_service_instances")
 	assert.NoError(t, err)
 }
