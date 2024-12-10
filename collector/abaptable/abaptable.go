c72ba17bdd126b8b76513f3cdfc9e1d407750816
wwsheng009 <vincentwwsheng@gmail.com>
2024-12-10 12:25:37 +0800

    add process percent metrix

--- collector/abaptable/abaptable.go
+++ collector/abaptable/abaptable.go
@@ -11,6 +11,23 @@ import (
 	log "github.com/sirupsen/logrus"
 )
 
+type ABAPProcessInfo struct {
+	dia_count int
+	dia_free  int
+	spo_count int
+	spo_free  int
+	btc_count int
+	btc_free  int
+	upd_count int
+	upd_free  int
+	up2_count int
+	up2_free  int
+}
+type abaptableServerCollector struct {
+	collector.DefaultCollector
+	webService sapcontrol.WebService
+}
+
 func NewCollector(webService sapcontrol.WebService) (*abaptableServerCollector, error) {
 
 	c := &abaptableServerCollector{
@@ -18,18 +35,15 @@ func NewCollector(webService sapcontrol.WebService) (*abaptableServerCollector, 
 		webService,
 	}
 
-	c.SetDescriptor("processes", "The abap work processes started by the SAP Start Service", []string{"no", "typ", "status", "reason", "start", "err", "sem", "cpu", "pid", "program", "client", "user", "action", "table", "instance_name", "instance_number", "SID", "instance_hostname"})
-	c.SetDescriptor("processes_dia", "The abap dia work processes started by the SAP Start Service", []string{"no", "typ", "status", "reason", "start", "err", "sem", "cpu", "pid", "program", "client", "user", "action", "table", "instance_name", "instance_number", "SID", "instance_hostname"})
-	c.SetDescriptor("processes_btc", "The abap btc work processes started by the SAP Start Service", []string{"no", "typ", "status", "reason", "start", "err", "sem", "cpu", "pid", "program", "client", "user", "action", "table", "instance_name", "instance_number", "SID", "instance_hostname"})
-	c.SetDescriptor("processes_spo", "The abap spo work processes started by the SAP Start Service", []string{"no", "typ", "status", "reason", "start", "err", "sem", "cpu", "pid", "program", "client", "user", "action", "table", "instance_name", "instance_number", "SID", "instance_hostname"})
-	c.SetDescriptor("processes_upd", "The abap upd work processes started by the SAP Start Service", []string{"no", "typ", "status", "reason", "start", "err", "sem", "cpu", "pid", "program", "client", "user", "action", "table", "instance_name", "instance_number", "SID", "instance_hostname"})
-	c.SetDescriptor("processes_up2", "The abap up2 work processes started by the SAP Start Service", []string{"no", "typ", "status", "reason", "start", "err", "sem", "cpu", "pid", "program", "client", "user", "action", "table", "instance_name", "instance_number", "SID", "instance_hostname"})
-	return c, nil
-}
+	c.SetDescriptor("processes_dia", "The abap dia work processes", []string{"no", "typ", "status", "reason", "start", "err", "sem", "cpu", "pid", "program", "client", "user", "action", "table", "instance_name", "instance_number", "SID", "instance_hostname"})
+	c.SetDescriptor("processes_btc", "The abap btc work processes", []string{"no", "typ", "status", "reason", "start", "err", "sem", "cpu", "pid", "program", "client", "user", "action", "table", "instance_name", "instance_number", "SID", "instance_hostname"})
+	c.SetDescriptor("processes_spo", "The abap spo work processes", []string{"no", "typ", "status", "reason", "start", "err", "sem", "cpu", "pid", "program", "client", "user", "action", "table", "instance_name", "instance_number", "SID", "instance_hostname"})
+	c.SetDescriptor("processes_upd", "The abap upd work processes", []string{"no", "typ", "status", "reason", "start", "err", "sem", "cpu", "pid", "program", "client", "user", "action", "table", "instance_name", "instance_number", "SID", "instance_hostname"})
+	c.SetDescriptor("processes_up2", "The abap up2 work processes", []string{"no", "typ", "status", "reason", "start", "err", "sem", "cpu", "pid", "program", "client", "user", "action", "table", "instance_name", "instance_number", "SID", "instance_hostname"})
+	c.SetDescriptor("processes_wait_pct", "The abap dia work processes free percent", []string{"typ", "instance_name", "instance_number", "SID", "instance_hostname"})
+	c.SetDescriptor("processes_simple", "The abap work processes", []string{"no", "typ", "pid", "instance_name", "instance_number", "SID", "instance_hostname"})
 
-type abaptableServerCollector struct {
-	collector.DefaultCollector
-	webService sapcontrol.WebService
+	return c, nil
 }
 
 func (c *abaptableServerCollector) Collect(ch chan<- prometheus.Metric) {
@@ -57,8 +71,17 @@ func (c *abaptableServerCollector) recordAbapTable(ch chan<- prometheus.Metric) 
 		currentSapInstance.Hostname,
 	}
 
+	info := ABAPProcessInfo{}
 	for _, process := range abapStatistic.Workprocess.Item {
-		labels := append([]string{strconv.Itoa(int(process.No)), process.Typ, process.Status, process.Reason, process.Start, process.Err, process.Sem, timeToSeconds(process.Cpu), strconv.Itoa(int(process.Pid)), process.Program, process.Client, process.User, process.Action, process.Table}, commonLabels...)
+		no := strconv.Itoa(int(process.No))
+		pid := strconv.Itoa(int(process.Pid))
+
+		labels := append([]string{no, process.Typ, process.Status, process.Reason, process.Start, process.Err, process.Sem,
+			timeToSeconds(process.Cpu),
+			pid, process.Program, process.Client, process.User, process.Action, process.Table}, commonLabels...)
+
+		// label的变化会导致 指标的变化， 所以需要单独增加一条简化的指标，删除一些会变化的字段
+		labels_simple := append([]string{no, process.Typ, pid}, commonLabels...)
 
 		floatValue, err := strconv.ParseFloat(process.Time, 64)
 		if err != nil {
@@ -67,18 +90,54 @@ func (c *abaptableServerCollector) recordAbapTable(ch chan<- prometheus.Metric) 
 		}
 		switch process.Typ {
 		case "DIA":
+			info.dia_count++
+			if process.Status == "Wait" {
+				info.dia_free++
+			}
 			ch <- c.MakeCounterMetric("processes_dia", floatValue, labels...)
 		case "BTC":
+			info.btc_count++
+			if process.Status == "Wait" {
+				info.btc_free++
+			}
 			ch <- c.MakeCounterMetric("processes_btc", floatValue, labels...)
 		case "SPO":
+			info.spo_count++
+			if process.Status == "Wait" {
+				info.spo_free++
+			}
 			ch <- c.MakeCounterMetric("processes_spo", floatValue, labels...)
 		case "UPD":
+			info.upd_count++
+			if process.Status == "Wait" {
+				info.upd_free++
+			}
 			ch <- c.MakeCounterMetric("processes_upd", floatValue, labels...)
 		case "UP2":
+			info.up2_count++
+			if process.Status == "Wait" {
+				info.up2_free++
+			}
 			ch <- c.MakeCounterMetric("processes_up2", floatValue, labels...)
 		default:
-			ch <- c.MakeCounterMetric("processes", floatValue, labels...)
 		}
+		ch <- c.MakeCounterMetric("processes_simple", floatValue, labels_simple...)
+
+	}
+	if info.dia_count > 0 {
+		ch <- c.MakeGaugeMetric("processes_wait_pct", float64(info.dia_free)/float64(info.dia_count)*100, append([]string{"ABAP/DIA"}, commonLabels...)...)
+	}
+	if info.spo_count > 0 {
+		ch <- c.MakeGaugeMetric("processes_wait_pct", float64(info.spo_free)/float64(info.spo_count)*100, append([]string{"ABAP/SPO"}, commonLabels...)...)
+	}
+	if info.btc_count > 0 {
+		ch <- c.MakeGaugeMetric("processes_wait_pct", float64(info.btc_free)/float64(info.btc_count)*100, append([]string{"ABAP/BTC"}, commonLabels...)...)
+	}
+	if info.upd_count > 0 {
+		ch <- c.MakeGaugeMetric("processes_wait_pct", float64(info.upd_free)/float64(info.upd_count)*100, append([]string{"ABAP/UPD"}, commonLabels...)...)
+	}
+	if info.up2_count > 0 {
+		ch <- c.MakeGaugeMetric("processes_wait_pct", float64(info.up2_free)/float64(info.up2_count)*100, append([]string{"ABAP/UP2"}, commonLabels...)...)
 	}
 	return nil
 }
